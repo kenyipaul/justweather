85f1d5ed9e042da837861d6448d6aab11804eb6f
Paul Kenyi <kenyipaul69@gmail.com>
2024-10-13 01:02:43 +0200

    -Re-designed folder structure

---
+++ src/components/forecasts.jsx
@@ -0,0 +1,25 @@
+export default function Forecasts() {
+    return (
+        <section className="footer">
+            
+            <div className="forecast-container">
+                <div className="top-bar">
+                    <p>Today</p>
+                    <p>Week</p>
+                </div>
+                <div className="forecast-list">
+                    <div className="forecast"></div>
+                    <div className="forecast"></div>
+                    <div className="forecast"></div>
+                    <div className="forecast"></div>
+                    <div className="forecast"></div>
+                    <div className="forecast"></div>
+                    <div className="forecast"></div>
+                    <div className="forecast"></div>
+                    <div className="forecast"></div>
+                </div>
+            </div>
+
+        </section>
+    )
+}
